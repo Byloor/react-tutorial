bb68d289f42ee761b05c253c556318549d17505b
LAPTOP-NTKDTAHG\yshal <yshali.byloor@gmail.com>
2021-03-21 01:17:19 +0100

    First basic app with nested components tutorial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// capitalise the function name for react to identify main function component
+// always return JSX, always return single element, a div/section/article or fragment wrap around
+//  This is a stateless functional component ( dumb components )
+// className instead of  class
+// close every element
+// formatting like a HTML - dont place everything as div, follow html semantics
+// use camelCase for html attribute
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Nested Components
+const Greeting = () => {
+  return (
+    <div className='main-wrapper'>
+      <PersonName />
+      <Message />
+    </div>
+  )
+}
+// OR ( not a nice method)
+// const Greeting = () => {
+//  return React.createElement('h2', {}, 'Hello world')
+// }
+
+const PersonName = () => <h1>John Doe</h1>
+const Message = () => <h4>Hello World, This is my first component</h4>
+
+ReactDOM.render(<Greeting></Greeting>, document.getElementById('root'))
